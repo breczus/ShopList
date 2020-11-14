908e9498ae0b4967cae45aebd300e4fd90efa2f5
breczus <breczus@gmail.com>
2020-11-14 21:58:58 +0100

    start project, add bootstrap to frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>
+          Shop List App
+        </h1>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
